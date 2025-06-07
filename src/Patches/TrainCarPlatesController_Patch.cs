a9709a55d7bad94cef36d48d53606c64419cef77
t0stiman <18124323+t0stiman@users.noreply.github.com>
2025-06-07 14:56:36 +0200

    Show fill % on traincar plates

---
+++ src/Patches/TrainCarPlatesController_Patch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DV.ThingTypes;
+using HarmonyLib;
+using UnityEngine;
+
+namespace better_loading.Patches;
+
+/// <summary>
+/// Show fill % on traincar plates
+/// </summary>
+[HarmonyPatch(typeof(TrainCarPlatesController))]
+[HarmonyPatch(nameof(TrainCarPlatesController.UpdateCargoData))]
+[HarmonyPatch(new Type[] {})]
+public class TrainCarPlatesController_UpdateCargoData_Patch 
+{
+	private static void Postfix(TrainCarPlatesController __instance)
+	{
+		if (
+			// __instance.trainCar.LoadedCargo != CargoType.Coal ||
+		    __instance.trainCar.logicCar == null)
+		{
+			return;
+		}
+
+		var capacity = __instance.trainCar.logicCar.capacity;
+		var loadedCargoAmount = __instance.trainCar.logicCar.LoadedCargoAmount;
+		
+		__instance.cargoMassText += $", {Mathf.RoundToInt(loadedCargoAmount / capacity * 100f)}%";
+		__instance.RefreshDerivedCargoJobData();
+	}
+}
