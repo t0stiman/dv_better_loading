001bc91a2b455a3e8f60c9f5f4e9408bf5854bcc
t0stiman <18124323+t0stiman@users.noreply.github.com>
2025-09-03 12:39:31 +0200

    ez v2 debugger

---
+++ src/Patches/TrainCar_Patch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace better_loading.Patches;
+
+[HarmonyPatch(typeof(TrainCar))]
+[HarmonyPatch(nameof(TrainCar.Awake))]
+public class TrainCar_Awake_Patch
+{
+	private static void Postfix(TrainCar __instance)
+	{
+		__instance.gameObject.AddComponent<TrainCarV2Debug>();
+	}
+}
---
+++ src/TrainCarV2Debug.cs
@@ -0,0 +1,30 @@
+﻿using DV.ThingTypes;
+using DV.ThingTypes.TransitionHelpers;
+using UnityEngine;
+
+namespace better_loading;
+
+public class TrainCarV2Debug: MonoBehaviour
+{
+	private TrainCar trainCar;
+	private CargoType previousLoadedCargo;
+	
+	public TrainCarType_v2 CarTypeV2;
+	public TrainCarLivery CarLivery;
+	public CargoType_v2 CargoTypeV2;
+	
+	private void Start()
+	{
+		trainCar = gameObject.GetComponent<TrainCar>();
+		CarLivery = trainCar.carType.ToV2();
+		CarTypeV2 = CarLivery.parentType;
+		previousLoadedCargo = CargoType.None;
+	}
+
+	private void Update()
+	{
+		if(trainCar.LoadedCargo == previousLoadedCargo) return;
+		CargoTypeV2 = trainCar.LoadedCargo.ToV2();
+		previousLoadedCargo = trainCar.LoadedCargo;
+	}
+}
